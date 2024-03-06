871de20980a49bd745f7ef34bf3f274161fd6789
mynameisleesiwon <seoil1221@gmail.com>
2024-03-06 20:07:20 +0900

    Firestore DB에서 실시간으로 쿼리의 변경사항 수신

--- src/components/timeline.tsx
+++ src/components/timeline.tsx
@@ -1,8 +1,15 @@
 import { useEffect, useState } from "react";
 import styled from "styled-components";
-import { collection, getDocs, orderBy, query } from "firebase/firestore";
+import {
+  collection,
+  limit,
+  onSnapshot,
+  orderBy,
+  query,
+} from "firebase/firestore";
 import { db } from "../firebase";
 import Tweet from "./tweet";
+import { Unsubscribe } from "firebase/auth";
 
 // ITweet 인터페이스는 트윗 데이터의 타입을 정의
 export interface ITweet {
@@ -15,44 +22,63 @@ export interface ITweet {
   createdAt: number;
 }
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  display: flex;
+  gap: 10px;
+  flex-direction: column;
+`;
 
 export default function Timeline() {
   const [tweets, setTweets] = useState<ITweet[]>([]);
 
-  // 트윗들을 가져오는 비동기 함수
-  const fetchTweets = async () => {
-    // 파이어베이스 쿼리를 생성
-    // 'tweets' 컬렉션에서 'createdAt' 필드에 따라 내림차순으로 정렬
-    const tweetsQuery = query(
-      collection(db, "tweets"),
-      orderBy("createdAt", "desc")
-    );
-
-    // 위에서 정의한 쿼리를 사용하여 문서들을 가져옴
-    const snapshot = await getDocs(tweetsQuery);
-
-    // 가져온 문서들(snapshot.docs)을 순회하며,
-    // 필요한 데이터를 추출하여 새로운 배열을 생성
-    const tweets = snapshot.docs.map((doc) => {
-      const { tweet, createdAt, userId, username, photo } = doc.data();
-
-      // 각 트윗의 데이터와 문서 ID를 반환
-      return {
-        tweet,
-        createdAt,
-        userId,
-        username,
-        photo,
-        id: doc.id,
-      };
-    });
-
-    setTweets(tweets);
-  };
-
   useEffect(() => {
+    // 구독을 해제할 수 있는 변수를 선언
+    // Unsubscribe 타입은 파이어베이스에서 제공하는 타입으로, 구독 해제 함수의 타입
+    let unsubscribe: Unsubscribe | null = null;
+
+    // 트윗들을 가져오는 비동기 함수를 선언
+    const fetchTweets = async () => {
+      // 파이어베이스에서 제공하는 query 함수를 사용하여 트윗들을 가져오는 쿼리를 생성
+      // 'tweets' 컬렉션에서 'createdAt' 필드를 기준으로 내림차순으로 정렬하고,
+      // 최대 25개의 문서만 가져오도록 설정
+      const tweetsQuery = query(
+        collection(db, "tweets"),
+        orderBy("createdAt", "desc"),
+        limit(25)
+      );
+
+      // onSnapshot 함수를 사용하여 실시간으로 데이터베이스의 변경사항을 감지
+      // tweetsQuery에 정의된 조건에 맞는 데이터가 변경될 때마다 콜백 함수가 호출
+      unsubscribe = await onSnapshot(tweetsQuery, (snapshot) => {
+        // snapshot.docs를 맵핑하여 각 문서(doc)의 데이터를 추출하고
+        // 필요한 정보만을 객체로 구성하여 새로운 tweets 배열을 만듬
+        const tweets = snapshot.docs.map((doc) => {
+          const { tweet, createdAt, userId, username, photo } = doc.data();
+
+          return {
+            tweet,
+            createdAt,
+            userId,
+            username,
+            photo,
+            id: doc.id, // 문서의 고유 ID도 객체에 포함
+          };
+        });
+
+        // 추출한 트윗 데이터로 상태를 업데이트
+        setTweets(tweets);
+      });
+    };
+
+    // 트윗들을 가져오는 함수를 실행
     fetchTweets();
+
+    // 컴포넌트가 언마운트될 때 실행
+    // 구독을 해제하는 함수가 존재한다면,
+    // 구독을 해제하여 불필요한 리소스 사용을 방지
+    return () => {
+      unsubscribe && unsubscribe();
+    };
   }, []);
 
   return (
